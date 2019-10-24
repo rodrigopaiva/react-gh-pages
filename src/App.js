2297e28de55ecd009fc3d80c195b06daa3b167fd
rodrigofpaiva <rodrigofpaiva@gmail.com>
2019-10-24 19:50:06 -0300

    filtro OK

--- src/App.js
+++ src/App.js
@@ -17,6 +17,129 @@ class App extends Component {
       items: [],
       dataRoute: 'http://rodrigofelipe.com.br/testes/wp-headless/wp-json/wp/v2/posts/'
     };
+
+    this.showAllItems = this.showAllItems.bind(this);
+    this.showEbooks   = this.showEbooks.bind(this);
+    this.showChart    = this.showChart.bind(this);
+    this.showWebinar  = this.showWebinar.bind(this);
+    this.showGear     = this.showGear.bind(this);
+  }
+
+
+  showAllItems() {
+    var a = document.querySelectorAll(".items li");
+    var j;
+    for (j = 0; j < a.length; j++) {
+      a[j].style.display = "block";
+    }
+
+    // add | remove class in menu
+    var elems = document.querySelectorAll(".nav-primary li");
+    [].forEach.call(elems, function(el) {
+      el.classList.remove("nav-active");
+    });
+
+    document.getElementById('nav-all-items').className = 'nav-active';
+  }
+
+
+  showEbooks() {
+    // hide list items
+    var a = document.querySelectorAll(".items li");
+    var j;
+    for (j = 0; j < a.length; j++) {
+      a[j].style.display = "none";
+    }
+
+    // show list items
+    var x = document.getElementsByClassName("e-books");
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "block";
+    }
+
+    // add | remove class in menu
+    var elems = document.querySelectorAll(".nav-primary li");
+    [].forEach.call(elems, function(el) {
+      el.classList.remove("nav-active");
+    });
+
+    document.getElementById('nav-books').className = 'nav-active';
+  }
+
+
+  showChart() {
+    // hide list items
+    var a = document.querySelectorAll(".items li");
+    var j;
+    for (j = 0; j < a.length; j++) {
+      a[j].style.display = "none";
+    }
+
+    // show list items
+    var x = document.getElementsByClassName("infográficos");
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "block";
+    }
+
+    // add | remove class in menu
+    var elems = document.querySelectorAll(".nav-primary li");
+    [].forEach.call(elems, function(el) {
+      el.classList.remove("nav-active");
+    });
+
+    document.getElementById('nav-chart').className = 'nav-active';
+  }
+
+
+  showWebinar() {
+    // hide list items
+    var a = document.querySelectorAll(".items li");
+    var j;
+    for (j = 0; j < a.length; j++) {
+      a[j].style.display = "none";
+    }
+
+    // show list items
+    var x = document.getElementsByClassName("webinar");
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "block";
+    }
+
+    // add | remove class in menu
+    var elems = document.querySelectorAll(".nav-primary li");
+    [].forEach.call(elems, function(el) {
+      el.classList.remove("nav-active");
+    });
+
+    document.getElementById('nav-webinar').className = 'nav-active';
+  }
+
+
+  showGear() {
+    // hide list items
+    var a = document.querySelectorAll(".items li");
+    var j;
+    for (j = 0; j < a.length; j++) {
+      a[j].style.display = "none";
+    }
+
+    // show list items
+    var x = document.getElementsByClassName("ferramentas");
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "block";
+    }
+
+    // add | remove class in menu
+    var elems = document.querySelectorAll(".nav-primary li");
+    [].forEach.call(elems, function(el) {
+      el.classList.remove("nav-active");
+    });
+
+    document.getElementById('nav-gear').className = 'nav-active';
   }
 
 
@@ -30,19 +153,19 @@ class App extends Component {
         <nav className="nav-primary animated bounceInDown delay-2s">
           <div className="aux center w-90">
             <ul>
-              <li className="nav-active" id="nav-all-items">
+              <li className="nav-active" onClick={this.showAllItems} id="nav-all-items">
                 <img src={iconStack} alt="icon-stack" /> todos
               </li>
-              <li id="nav-books">
+              <li onClick={this.showEbooks} id="nav-books">
                 <img src={iconBook} alt="icon-book" /> e-books
               </li>
-              <li id="nav-chart">
+              <li onClick={this.showChart} id="nav-chart">
                 <img src={iconChart} alt="icon-chart" /> infográficos
               </li>
-              <li id="nav-webinar">
+              <li onClick={this.showWebinar} id="nav-webinar">
                 <img src={iconPlay} alt="icon-play" /> webinar
               </li>
-              <li id="nav-gear">
+              <li onClick={this.showGear} id="nav-gear">
                 <img src={iconGear} alt="icon-gear" /> ferramentas
               </li>
             </ul>

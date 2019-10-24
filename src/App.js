90094cf435c9781418dffa74e7d24a8209cfd82e
rodrigofpaiva <rodrigofpaiva@gmail.com>
2019-10-24 19:45:00 -0300

    navigation OK

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import iconBook from './images/book.svg';
+import iconChart from './images/chart.svg';
+import iconGear from './images/gear.svg';
+import iconPlay from './images/play.svg';
+// import iconPlus from './images/plus.svg';
+import iconStack from './images/stack.svg';
+import iconHappy from './images/emoji-happy.png';
+import './css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      items: [],
+      dataRoute: 'http://rodrigofelipe.com.br/testes/wp-headless/wp-json/wp/v2/posts/'
+    };
+  }
+
+
+
+  render() {
+
+    return (
+
+      <div className="app">
+
+        <nav className="nav-primary animated bounceInDown delay-2s">
+          <div className="aux center w-90">
+            <ul>
+              <li className="nav-active" id="nav-all-items">
+                <img src={iconStack} alt="icon-stack" /> todos
+              </li>
+              <li id="nav-books">
+                <img src={iconBook} alt="icon-book" /> e-books
+              </li>
+              <li id="nav-chart">
+                <img src={iconChart} alt="icon-chart" /> infográficos
+              </li>
+              <li id="nav-webinar">
+                <img src={iconPlay} alt="icon-play" /> webinar
+              </li>
+              <li id="nav-gear">
+                <img src={iconGear} alt="icon-gear" /> ferramentas
+              </li>
+            </ul>
+
+            <h1>
+              Materiais gratuitos <img src={ iconHappy } alt="icon-happy" />
+            </h1>
+          </div>
+        </nav>
+
+      </div>
+    );
+  }
+
+
+  componentDidMount(){
+    fetch(this.state.dataRoute)
+      .then(res => res.json())
+      .then(items => this.setState((prevState, props) => {
+        return { items: items.map(this.mapitem)};
+      }));
+  }
+
+
+  mapitem(item){
+    return {
+      id: item.id,
+      image: item.acf_fields.image,
+      name: item.title.rendered,
+      link: item.link,
+      description: item.acf_fields.description,
+      category: item.acf_fields.type
+    }
+  }
 }
 
 export default App;
